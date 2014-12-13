71fa1eb4b2c4adcb75b6098f5129b43bcdcb0515
SofiaReis <sofiareis1994@gmail.com>
2014-12-13 23:42:24 +0000

    Almost finished

--- easy_as_abc.pl
+++ easy_as_abc.pl
@@ -1,21 +1,14 @@
 % ***************************************************************************
-% *                                                                         *
-% *                             Neutron                                     *
-% *                                                                         *
+% *                              EASY_AS_ABC                                *
 % ***************************************************************************
 % * Realizado por:                                                          *
 % *            Maria Marques  - ei12104@fe.up.pt                            *
 % *            Sofia Reis - ei12041@fe.up.pt                        MIEIC14 *
 % ***************************************************************************
-% *                              Bibliotecas                                *                      
-% ***************************************************************************
 :-use_module(library(clpfd)).
 :-use_module(library(lists)).
 
-% ***************************************************************************
-% *                            Implementação                                *                      
-% ***************************************************************************
-
+% Generates a dinamic board
 gentab([],_).
 gentab([H|T],S):-
         length(H, S),
@@ -24,12 +17,18 @@ gentab([H|T],S):-
         all_different(H),
         gentab(T,S).
 
+% Constraints
 clueM([],[]).
 clueM([[H1,H2|_]|T],[Ch|Ct]):-
         Ch #= 0 #\/ Ch #= H1 #\/ (H1 #= 0 #/\ Ch #= H2),
         clueM(T,Ct).
 
+% Solution
 solve(L1,L2,L3,L4,Vars):-
+        %conv(L1, NL1),
+        %conv(L2,NL2),
+        %conv(L3,NL3),
+        %conv(L4,NL4),
         length(L1, S),
         length(Vars, S),
         gentab(Vars,S),
@@ -43,34 +42,71 @@ solve(L1,L2,L3,L4,Vars):-
         transpose(RVars, TRVars),
         clueM(TRVars,L4),
         append(Vars,L),
-        labeling([],L).
+        labeling([],L),  
+        print_end(L1,L2,L3,L4,Vars).
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-escreve(0):-write('   ').
-escreve(1):-write(' A ').
-escreve(2):-write(' B ').
-escreve(3):-write(' C ').
-escreve(4):-write(' D ').
-escreve(5):-write(' E ').
-escreve(6):-write(' F ').
-escreve(7):-write(' G ').
-escreve(8):-write(' H ').
-escreve(9):-write(' I ').
-escreve(10):-write(' J ').
-escreve(11):-write(' K ').
-escreve(12):-write(' L ').
-escreve(13):-write(' M ').
-escreve(14):-write(' N ').
-escreve(15):-write(' O ').
-escreve(16):-write(' P ').
-escreve(17):-write(' Q ').
-escreve(18):-write(' R ').
-escreve(19):-write(' S ').
-escreve(20):-write(' T ').
-escreve(21):-write(' U ').
-escreve(22):-write(' V ').
-escreve(23):-write(' W ').
-escreve(24):-write(' X ').
-escreve(25):-write(' Y ').
-escreve(26):-write(' Z ').
+%converte numbers->letters
+conv([],_).
+conv([L|T],NL):-
+        convert(A,L),
+        append(NL,[A],NewNL),
+        conv(T,NewNL).
+
+% print final board
+print_end(L1,L2,L3,L4,Vars):-
+        nl,
+        write(' '),print_clue(L2),nl,
+        print_tab(0,Vars,L1,L3),
+        write(' '),print_clue(L4),
+        !.
+
+print_clue([]).
+print_clue([H|L]) :-
+        write(' '),
+        convert(H,A),write(A),
+        print_clue(L).
+        
+print_line([]).
+print_line([H|L]) :-
+        write('|'),
+        convert(H,A),write(A),
+        print_line(L).
+
+print_tab(_,[],[],[]).
+print_tab(N,[H|L],[L1|R1],[L3|R3]) :-
+        convert(L1,A), write(A),
+        print_line(H), write('|'), 
+        convert(L3,B), write(B),
+        nl,
+        N2 is N+1,
+        print_tab(N2,L,R1,R3).
+
+% Traduction of numbers in letters
+convert(0,' ').
+convert(1,'A').
+convert(2,'B').
+convert(3,'C').
+convert(4,'D').
+convert(5,'E').
+convert(6,'F').
+convert(7,'G').
+convert(8,'H').
+convert(9,'I').
+convert(10,'J').
+convert(11,'K').
+convert(12,'L').
+convert(13,'M').
+convert(14,'N').
+convert(15,'O').
+convert(16,'P').
+convert(17,'Q').
+convert(18,'R').
+convert(19,'S').
+convert(20,'T').
+convert(21,'U').
+convert(22,'V').
+convert(23,'W').
+convert(24,'X').
+convert(25,'Y').
+convert(26,'Z').
